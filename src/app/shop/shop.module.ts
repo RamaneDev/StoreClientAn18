f59f30dbbb3b30143a7d96b84a206ec96ce751e0
mesri <abde_mes@yahoo.fr>
2025-02-09 18:11:40 +0100

    Adding modules + comp

---
+++ src/app/shop/shop.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShopRoutingModule } from './shop-routing.module';
+import { ShopComponent } from './shop/shop.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+
+
+@NgModule({
+  declarations: [
+    ShopComponent,
+    ProductItemComponent
+  ],
+  imports: [
+    CommonModule,
+    ShopRoutingModule
+  ]
+})
+export class ShopModule { }
